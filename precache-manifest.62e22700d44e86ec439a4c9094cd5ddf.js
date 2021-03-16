66fd195e25fb34d17bae98048bb12c643383b80e
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-03-16 13:59:40 +0200

    Updates

---
+++ precache-manifest.62e22700d44e86ec439a4c9094cd5ddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c692db9b8479ed89d52c28ba24da05c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "282e4d5b13511f3c21fa",
+    "url": "./static/css/main.9ca7d800.chunk.css"
+  },
+  {
+    "revision": "f605fb10a39f70f0b262",
+    "url": "./static/js/2.c9d11b7c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c9d11b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282e4d5b13511f3c21fa",
+    "url": "./static/js/main.5fb103aa.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
